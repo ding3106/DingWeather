8573f06afb32d1a77635102062620f877beb1c2c
ding3106 <ding3106@126.com>
2018-08-21 18:22:28 +0800

    Second Commit
    create db config file litepal.xml
    create db classes:Province, City, County
    Config AndroidManifest.xml about application's name

---
+++ app/src/main/java/com/lenovo/dingjq1/dingweather/db/Province.java
@@ -0,0 +1,35 @@
+package com.lenovo.dingjq1.dingweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
