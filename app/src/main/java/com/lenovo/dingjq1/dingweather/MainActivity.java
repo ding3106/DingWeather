6b087c8b35f57a1fa14e7327b3e2a544b7308334
ding3106 <ding3106@126.com>
2018-08-21 17:34:10 +0800

    Initial commit

---
+++ app/src/main/java/com/lenovo/dingjq1/dingweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lenovo.dingjq1.dingweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
